b4da1a9a2230e889baadf71560b765787e1be0c6
Anders Holmberg <anders@hiddenincome.com>
2019-01-05 14:44:31 +0100

    Updated oprint (opinionated print)
    Function now takes either an array of variables or a single variable. It can also print objects (it only prints the script members, not inherited members).

--- util.gd
+++ util.gd
@@ -1,35 +1,51 @@
 extends Node
 
-func oprint(format, var_array):
-	
+static func convert_base_type(var_item):
+	match typeof(var_item):
+		TYPE_NIL:
+			return 'null'
+		TYPE_BOOL:
+			return str(var_item)
+		TYPE_INT:
+			return str(var_item)
+		TYPE_REAL:
+			return '%.3f' % [var_item]
+		TYPE_STRING:
+			return '"%s"' % [var_item]
+		TYPE_VECTOR2:
+			return 'v2(%.3f, %.3f)' % [var_item.x, var_item.y]
+		TYPE_OBJECT:
+			var res = '%s { ' % [var_item.name]
+			for i in var_item.get_property_list():
+				if i.usage == PROPERTY_USAGE_SCRIPT_VARIABLE:
+					var value = var_item.get(i.name)
+					res += '%s: %s ' % [i.name, convert_base_type(value)]
+			res += '}'
+			return res
+
+	return '[Unsupported type %d]' % [typeof(var_item)]
+
+static func oprint(format, var_item_or_array):
+
 	var resolved_items = []
-	
-	for var_item in var_array:		
-		match typeof(var_item):
-			TYPE_NIL:
-				resolved_items.append('null')
-			TYPE_BOOL:
-				resolved_items.append(str(var_item))
-			TYPE_INT:
-				resolved_items.append(str(var_item))
-			TYPE_REAL:
-				resolved_items.append('%.4f' % [var_item])
-			TYPE_STRING:
-				resolved_items.append('"%s"' % [var_item])
-			TYPE_VECTOR2:
-				resolved_items.append('v2(%.4f, %.4f)' % [var_item.x, var_item.y])
-			_:
-				resolved_items.append('[Unsupported type %d]' % [typeof(var_item)])
-				
+
+	match typeof(var_item_or_array):
+		TYPE_ARRAY:
+			for var_item in var_item_or_array:
+				resolved_items.append(convert_base_type(var_item))
+		_:
+			resolved_items.append(convert_base_type(var_item_or_array))
+
 	var resolved_item_index = 0
 	var res = ''
-	
-	# @Incomplete - no error checking...
-	for c in format:		
+
+	# @Incomplete - no error checking, simply skipping items if '%' are missing...
+	for c in format:
 		if c == '%':
-			res += resolved_items[resolved_item_index]
-			resolved_item_index += 1
+			if resolved_item_index < len(resolved_items):
+				res += resolved_items[resolved_item_index]
+				resolved_item_index += 1
 		else:
 			res += c
-			
+
 	print(res)
